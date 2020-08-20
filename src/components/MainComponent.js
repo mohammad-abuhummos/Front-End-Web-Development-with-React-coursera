ea4c945ae105e3e29bf054e586c25ae59bf4fe81
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-08-21 01:47:28 +0300

    single page application part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from "react-router-dom";
+import DishDetail from "./DishdetailComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -32,6 +33,13 @@ class Main extends Component {
       );
     }
     
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                        comments={this.state.comments.filter((comment) => comment.dishId=== parseInt(match.params.dishId,10))}
+            />
+        );
+    }
     return (
       <div>
         <Header />
@@ -42,6 +50,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+           <Route  path='/menu/:dishId' component={DishWithId} /> 
            <Route exact path='/contactus' component={Contact} /> 
           <Redirect to="/home" />
         </Switch>
